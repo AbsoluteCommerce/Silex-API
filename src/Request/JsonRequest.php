703122c119a6ae97e464cd0d83e6332c20b5ee32
Joseph McDermott <code@josephmcdermott.co.uk>
2017-03-14 17:31:41 +0000

    Added handling of nested params like name[value1][value2] to JsonRequest

--- src/Request/JsonRequest.php
+++ src/Request/JsonRequest.php
@@ -13,7 +13,44 @@ class JsonRequest implements RequestInterface
      */
     public function getQuery(HttpRequest $httpRequest, string $field)
     {
-        return $httpRequest->get($field);
+        #todo need unit tests for this
+        
+        // handle nested[para][meters]
+        parse_str($field, $result);
+        
+        $field = key($result);
+        $value = reset($result);
+        $data = $httpRequest->get($field);
+        
+        if ($data === null) {
+            return null;
+        } elseif (!is_array($value)) {
+            return $data;
+        } else {
+            $nestedFields = reset($result);
+            $nestedValue = $this->_getNestedQueryData($nestedFields, $data);
+            return $nestedValue;
+        }
+    }
+
+    /**
+     * @param array $nestedFields
+     * @param array $data
+     * @return mixed
+     */
+    private function _getNestedQueryData($nestedFields, $data)
+    {
+        $dataKey = key($nestedFields);
+        $childFields = reset($nestedFields);
+        
+        if (empty($childFields)) {
+            return array_key_exists($dataKey, $data)
+                ? $data[$dataKey]
+                : null;
+        } else {
+            
+            return $this->_getNestedQueryData($childFields, $data[$dataKey]);
+        }
     }
 
     /**
